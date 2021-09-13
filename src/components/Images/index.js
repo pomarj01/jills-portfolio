316995dbbf6679c3c0d7f86c4c3167aa62f60eff
Jill <pomarj01@gmail.com>
2021-09-13 17:14:44 +0100

    Create collection of images

---
+++ src/components/Images/index.js
@@ -0,0 +1,65 @@
+import webd from '../../assets/webD.svg';
+import vertical_crosses from '../../assets/vertical_crosses.svg';
+import double_waves from '../../assets/double_waves.svg';
+import lined_circle from '../../assets/lined_circle.svg';
+import rounded_wave from '../../assets/rounded_wave.svg';
+import tiny_circles from '../../assets/tiny_circles.svg';
+import circles from '../../assets/circles.svg';
+import pink_cross from '../../assets/pink_cross.svg';
+import blue_cross from '../../assets/blue_cross.svg';
+import dots_oblong from '../../assets/dots.svg';
+import zigzag from '../../assets/zigzag.svg';
+
+
+const image = [
+  {
+    name: "main_image",
+    value: `${webd}`,
+  },
+  {
+    name: "vertical_crosses",
+    value: `${vertical_crosses}`,
+  },
+  {
+    name: "double_waves",
+    value: `${double_waves}`,
+  },
+  {
+    name: "lined_circle",
+    value: `${lined_circle}`,
+  },
+  {
+    name: "rounded_wave",
+    value: `${rounded_wave}`,
+  },
+  {
+    name: "tiny_circles",
+    value: `${tiny_circles}`,
+  },
+  {
+    name: "circles",
+    value: `${circles}`,
+  },
+  {
+    name: "pink_cross",
+    value: `${pink_cross}`,
+  },
+  {
+    name: "blue_cross",
+    value: `${blue_cross}`,
+  },
+  {
+    name: "dots_oblong",
+    value: `${dots_oblong}`,
+  },
+  {
+    name: "zigzag",
+    value: `${zigzag}`,
+  },
+
+];
+
+
+export {
+  image,
+};
