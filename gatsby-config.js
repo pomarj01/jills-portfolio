4d4a62be275ec72c443534e4de2bd3dcbcc8704a
Jill <pomarj01@gmail.com>
2021-09-15 22:25:28 +0100

    Add navigation link object inside config to use GraphQL query

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,21 @@
 module.exports = {
+  siteMetadata: {
+    title: "Jill Pomares's Portfolio",
+    navLinks: [
+      {
+        name: "Home",
+        link: "/",
+      },
+      {
+        name: "About",
+        link: "/dev",
+      },
+      {
+        name: "Projects",
+        link: "/projects"
+      },
+    ],
+  },
   plugins: [
     "gatsby-plugin-styled-components",
     "gatsby-plugin-react-helmet"
