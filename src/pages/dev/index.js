4075223ca6e5743651bd117d6600bc84dfa67127
Jill <pomarj01@gmail.com>
2021-09-16 18:21:33 +0100

    Create dev and projects component

---
+++ src/pages/dev/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+import Navigation from "../../components/navigation";
+
+const Wrapper = styled.div`
+  background: #252835;
+  padding: 0 1rem;
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const DevPage = ({ location }) => {
+  const hideNav = location.pathname === "/";
+
+
+  return (
+    <Wrapper id='test'>
+      {hideNav ? (
+        <h1>this is the content</h1>
+      ) : (
+        <>
+          <Navigation location={location} />
+          <p>should show navigation</p>
+        </>
+      )}
+    </Wrapper>
+  );
+}
+
+export default DevPage;
