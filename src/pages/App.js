f7e1bba6ef2d9e07877d1023ac0747e8eb410e26
Jill <pomarj01@gmail.com>
2021-10-11 16:12:40 +0100

    Remove dev component

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 import Hero from './hero';
-import Dev from './dev';
+import About from './about';
 import Projects from './projects';
 
 const Wrapper = styled.div`
   background: #252835;
-  padding: 0 1rem;
 `;
 
 const App = ({ location }) => (
   <Wrapper>
     <Hero location={location} />
-    <Dev location={location} />
+    <About location={location} />
     <Projects location={location} />
   </Wrapper>
   );
