be8bc0e4524543dde08064d430f0633f24722af2
Jill <pomarj01@gmail.com>
2021-10-18 16:47:34 +0100

    Imported more assets in the Images library

--- src/components/images/Images.js
+++ src/components/images/Images.js
@@ -1,3 +1,4 @@
+import logo from '../../assets/logo.svg';
 import webd from '../../assets/webD.svg';
 import vertical_crosses from '../../assets/vertical_crosses.svg';
 import double_waves from '../../assets/double_waves.svg';
@@ -14,17 +15,43 @@ import dev_illustration2 from '../../assets/dev_illustration2.svg';
 import des_illustration from '../../assets/des_illustration.svg';
 import des_monitor from "../../assets/design_monitor.svg";
 import dev_monitor from "../../assets/dev_monitor.svg";
-import cloud from "../../assets/cloud.svg";
+import half_cloud from "../../assets/cloud.svg";
 import splash from "../../assets/splash.svg";
 
 import oblongs from "../../assets/oblongs.svg";
 import eclipses from "../../assets/eclipses.svg";
 import big_waves from "../../assets/big_waves.svg";
+import square_dots from "../../assets/square_dots.svg";
+import filled_oblongs from "../../assets/filled_oblongs.svg";
+import triangle_circle from "../../assets/triangle_circle.svg";
+import circledLines_dots from "../../assets/circledLines_dots.svg";
+import monitor from '../../assets/icons/monitor.svg'
+import pen from '../../assets/icons/pen.svg'
+import palette from '../../assets/icons/palette.svg'
+import wireframe from '../../assets/icons/wireframe.svg'
+import vector from '../../assets/icons/vector.svg'
+import ab_testing from "../../assets/icons/ab_testing.svg";
+import cloud from "../../assets/icons/cloud.svg";
+import github from "../../assets/icons/github.svg";
+import js from "../../assets/icons/js.svg";
+import vscode from "../../assets/icons/vscode.svg";
+import spaceship from "../../assets/icons/spaceship.svg";
+import planet from "../../assets/icons/planet.svg";
+import paint_roller from "../../assets/icons/paint_roller.svg";
+import letterJ from "../../assets/icons/letterJ.svg";
+import letterP from "../../assets/icons/letterP.svg";
+
 import waitron_dashboard from "../../assets/waitron_dashboard.png";
 import fb_landing from "../../assets/fb_landing.png";
 import fb_register from "../../assets/fb_register.png";
+import heattech_homepage from "../../assets/heattech_homepage.png";
+import heattech_homepage_2 from "../../assets/heattech_homepage_2.png";
 
 const images = [
+  {
+    name: "logo",
+    url: `${logo}`,
+  },
   {
     name: "main_image",
     url: `${webd}`,
@@ -70,6 +97,94 @@ const images = [
     url: `${zigzag}`,
   },
   {
+    name: "oblongs",
+    url: `${oblongs}`,
+  },
+  {
+    name: "eclipses",
+    url: `${eclipses}`,
+  },
+  {
+    name: "big_waves",
+    url: `${big_waves}`,
+  },
+  {
+    name: "square_dots",
+    url: `${square_dots}`,
+  },
+  {
+    name: "filled_oblongs",
+    url: `${filled_oblongs}`,
+  },
+  {
+    name: "triangle_circle",
+    url: `${triangle_circle}`,
+  },
+  {
+    name: "circledLines_dots",
+    url: `${circledLines_dots}`,
+  },
+  {
+    name: "monitor",
+    url: `${monitor}`,
+  },
+  {
+    name: "palette",
+    url: `${palette}`,
+  },
+  {
+    name: "pen",
+    url: `${pen}`,
+  },
+  {
+    name: "vector",
+    url: `${vector}`,
+  },
+  {
+    name: "ab_testing",
+    url: `${ab_testing}`,
+  },
+  {
+    name: "cloud",
+    url: `${cloud}`,
+  },
+  {
+    name: "github",
+    url: `${github}`,
+  },
+  {
+    name: "js",
+    url: `${js}`,
+  },
+  {
+    name: "vscode",
+    url: `${vscode}`,
+  },
+  {
+    name: "wireframe",
+    url: `${wireframe}`,
+  },
+  {
+    name: "planet",
+    url: `${planet}`,
+  },
+  {
+    name: "paint_roller",
+    url: `${paint_roller}`,
+  },
+  {
+    name: "spaceship",
+    url: `${spaceship}`,
+  },
+  {
+    name: "letterJ",
+    url: `${letterJ}`,
+  },
+  {
+    name: "letterP",
+    url: `${letterP}`,
+  },
+  {
     name: "dev_illustration",
     url: `${dev_illustration}`,
   },
@@ -82,20 +197,8 @@ const images = [
     url: `${des_illustration}`,
   },
   {
-    name: "oblongs",
-    url: `${oblongs}`,
-  },
-  {
-    name: "eclipses",
-    url: `${eclipses}`,
-  },
-  {
     name: "waitron_dashboard",
     url: `${waitron_dashboard}`,
-  },
-  {
-    name: "big_waves",
-    url: `${big_waves}`,
   },
   {
     name: "fb_landing",
@@ -104,6 +207,14 @@ const images = [
   {
     name: "fb_register",
     url: `${fb_register}`,
+  },
+  {
+    name: "heattech",
+    url: `${heattech_homepage}`,
+  },
+  {
+    name: "heattech_2",
+    url: `${heattech_homepage_2}`,
   },
 ];
 
