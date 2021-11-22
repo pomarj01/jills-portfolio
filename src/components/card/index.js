f40dc446220a9490f63ccb2733884469ecc80f09
Jill <pomarj01@gmail.com>
2021-11-22 12:35:05 +0000

    Add additional props on Card component for using custom buttons

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -2,20 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Button from "../button";
-import { device } from "../../utils";
+import { device, theme } from "../../utils";
 
 const DefaultCard = styled.div`
   background: ${(props) => props.background};
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin: 0 2rem;
-  padding: 1rem;
+  margin: ${theme.space[0]} ${theme.space[9]};
+  padding: ${theme.space[5]};
   width: 100%;
-  border-radius: 5px;
+  border-radius: ${theme.radii.base};
 
   @media ${device.laptop} {
-    padding: 4rem 2rem;
+    padding: ${theme.space[10]} ${theme.space[9]};
     width: 60%;
   }
 `;
@@ -24,27 +24,27 @@ const CardHeader = styled.div`
   display: flex;
   justify-content: center;
   width: 100%;
-  padding: 1rem 0;
+  padding: ${theme.space[5]} ${theme.space[0]};
 
   @media ${device.tablet} {
-    padding: 1rem;
-    margin-bottom: 1rem;
+    padding: ${theme.space[5]};
+    margin-bottom: ${theme.space[5]};
   }
 `;
 
 const Header = styled.h2`
   min-height: 0vw;
   font-size: clamp(1.25rem, 0.25rem + 3.3333vw, 2rem);
-  font-weight: 700;
+  font-weight: ${theme.fontWeights.bold};
 `;
 
 const CardDescription = styled.div`
   display: flex;
-  padding: 1rem;
+  padding: ${theme.space[5]};
   text-align: center;
 
   @media ${device.laptop} {
-    padding: 1rem 4rem;
+    padding: ${theme.space[5]} ${theme.space[10]};
   }
 `;
 
@@ -62,17 +62,18 @@ const CardTags = styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
-    padding: 1rem;
+    padding: ${theme.space[5]};
   }
 `;
 
 const Tag = styled.div`
   background: ${(props) => props.color};
   font-size: 12px;
-  font-weight: 800;
-  padding: 8px 16px;
-  margin: 0 0.5rem 0.5rem 0;
-  border-radius: 50px;
+  font-weight: ${theme.fontWeights.bold};
+  padding: ${theme.space[3]} ${theme.space[5]};
+  margin-right: ${theme.space[3]};
+  margin-bottom: ${theme.space[3]};
+  border-radius: ${theme.radii.round};
 `;
 
 const CardMedia = styled.div`
@@ -80,7 +81,7 @@ const CardMedia = styled.div`
   flex-direction: row;
   justify-content: center;
   width: 100%;
-  padding: 1rem;
+  padding: ${theme.space[5]};
   ${(props) => props.background !== 'none' ? `background: ${props.background}` : ''};
 `;
 
@@ -88,9 +89,9 @@ const CardMedia = styled.div`
 const Image = styled.img`
   width: 100%;
   overflow: hidden;
-  border-radius: 5px;
+  border-radius: ${theme.radii.base};
   box-shadow: 0px 50px 100px rgba(92, 99, 105, 0.15);
-  margin-left: 0.5rem;
+  margin-left: ${theme.space[3]};
 
   &:not(:first-child) {
     display: none;
@@ -104,17 +105,6 @@ const Image = styled.img`
     &:not(:first-child) {
       display: flex;
     }
-  }
-`;
-
-const Divider = styled.span`
-  border-bottom: 1px solid #252835;
-  width: 100%;
-  padding: 1rem;
-  order: 1;
-
-  @media ${device.tablet} {
-    display: none;
   }
 `;
 
@@ -144,6 +134,9 @@ const BaseCard = (props) => {
     cta,
     ctaContent,
     ctaDisabledButton,
+    ctaVariant,
+    customColor,
+    customText,
     ctaOnClick
   } = props;
 
@@ -161,25 +154,34 @@ const BaseCard = (props) => {
       <CardTags>
         {tags &&
           tags.map((tag, i) => (
-            <Tag key={i} color={tagsColor}>
-              {tag || "Tag not provided"}
-            </Tag>
-          ))}
+            <Tag 
+              key={i}
+              color={tagsColor}
+            > {tag} </Tag>
+          )) ||
+          <span> Tag not provided </span>}
       </CardTags>
 
       <CardMedia background={mediaBackground} style={mediaWrapperStyles}>
-        {(media &&
+        {media &&
           media.map((img, i) => (
-            <Image src={img} key={i} size={mediaSize} style={imageStyles} />
-          ))) || <span>Image not provided</span>}
+            <Image 
+              src={img}
+              key={i}
+              size={mediaSize}
+              style={imageStyles}
+            />
+          )) || <span> Image not provided </span>}
       </CardMedia>
 
       {cta && (
         <Button
-          variant="primary"
+          variant={ctaVariant}
           label={ctaContent}
           disabled={ctaDisabledButton}
           onClick={ctaOnClick}
+          customText={customText}
+          customColor={customColor}
         />
       )}
     </DefaultCard>
@@ -196,12 +198,14 @@ BaseCard.propTypes = {
   mediaBackground: PropTypes.string,
   mediaSize: PropTypes.string,
   cta: PropTypes.bool,
+  ctaVariant: PropTypes.string,
 };
 
 BaseCard.defaultProps = {
   mediaSize: "100%",
   mediaBackground: "none",
   cta: true,
+  ctaVariant:"primary"
 };
 
 export default BaseCard ;
