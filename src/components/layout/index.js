cc176d0ed3c6de730b47734ac7bfcf80968759ff
Jill <pomarj01@gmail.com>
2021-11-22 10:55:02 +0000

    Create footer & place it in the same file with header in layout

---
+++ src/components/layout/index.js
@@ -0,0 +1,120 @@
+import React from "react";
+import styled from "styled-components";
+import Navigation from "../navigation";
+import Button from "../button";
+import { images, icons } from "../../components/images/library";
+import { theme, device } from '../../utils'
+
+const footerSVG = images("footer_wave");
+
+const Wrapper = styled.div`
+  background: url(${footerSVG});
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  background-position: 80% 20%;
+  background-size: 220%;
+  background-repeat: no-repeat;
+  padding-top: ${theme.space[9]};
+
+  @media ${device.tablet} {
+    background-position: 50% 40%;
+    background-size: 100%;
+  }
+
+  @media ${device.l_laptop} {
+    background-position: 50% 20%;
+    padding-top: ${theme.space[11]};
+    margin-top: ${theme.space[9]};
+  }
+`;
+
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: auto;
+  padding: ${theme.space[10]} ${theme.space[0]} ${theme.space[5]};
+  
+  @media ${device.tablet} {
+    padding: ${theme.space[11]} ${theme.space[0]} ${theme.space[9]};
+  }
+`;
+
+const Content = styled.div`
+  font-size: clamp(1.5rem, 0.6429rem + 1.1607vw, 2.5rem);
+  font-weight: ${theme.fontWeights.bold};
+`;
+
+const FooterBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  background: ${theme.colors.accent[0]};
+  height: 68px;
+  padding: 0 1rem;
+`;
+
+const Author = styled.span`
+  font-weight: ${theme.fontWeights.bold};
+  display: inherit;
+
+  img {
+    padding: 0 8px;
+    width: 30px;
+  }
+`;
+
+const SocialMedia = styled.div`
+  display: flex;
+
+  img {
+    width: 20px;
+  }
+
+  img:first-child {
+    margin-right: ${theme.space[5]};
+  }
+`;
+
+
+export function Header({ children }) {
+  return (
+    <>
+      <Navigation />
+      {children}
+    </>
+  );
+};
+
+
+export function Footer() {
+  return (
+    <Wrapper>
+      <ContentWrapper>
+        <Content>Wanna chat?</Content>
+        <Button
+          variant="custom"
+          customColor="#252835"
+          customText="#CEACB7"
+        >
+          Contact me
+        </Button>
+      </ContentWrapper>
+      <FooterBar>
+        <Author>
+          Developed with
+          <img src={icons("heart")} alt="heart icon" />
+          by Jill Pomares
+        </Author>
+        <SocialMedia>
+          <img src={icons("github_icon")} alt="github icon" />
+          <img src={icons("linkedIn")} alt="linkedin icon" />
+        </SocialMedia>
+      </FooterBar>
+    </Wrapper>
+  );
+};
